791d553442719e168849a99e12c50a2721550f36
seek1r <2005cwj@gmail.com>
2025-02-09 19:49:51 -0500

    feature(iter0): parsing tool database connection
    - created a firestore database
    - created a program with firebase functionalities called 'firebase.js'
    - connected the firestore database to 'parser.js' using 'firebase.js'
    - formatted data extracted from course site html can now be stored in db.

---
+++ backend/src/collect.js
@@ -0,0 +1,125 @@
+/* 
+   This console application accesses and collects PUBLIC data from York University's 
+   Course Description Module (CDM). This program fully controls the scraping bot
+   and may send collected data to other programs to be processed and sent to a database.
+
+  To accomplish such data collection, Google's Puppeteer JS library was with some plugins.
+
+  > I think the program does mostly work in its current state
+  > Need to remove login process
+  > Need to actually extract data
+  > Work on iteration process
+  > need more thorough commenting
+*/
+
+
+// Dependencies 
+const { connect } = require('puppeteer-real-browser');
+
+function sleep(ms) { return new Promise((resolve) => { setTimeout(resolve, ms)})}
+
+//function that ensures a page is reached. 
+async function ensurePage(page, pagename) {
+  let pt = await page.title();
+  
+  while (pt != pagename) {
+    pt = null;
+    pt = await page.title();
+    sleep(500);
+  }
+  pt = null;
+}
+//program 'main' function
+async function start() {
+  // init puppeteer instance
+  const { browser, page } = await connect({
+    args: ["--start-maximized",
+      '--disable-infobars'
+    ],
+    turnstile: true,
+    headless: false,
+    customConfig: {},
+
+    plugins: [
+      require('puppeteer-extra-plugin-stealth')()
+    ],
+    connectOption: {
+      defaultViewport: null
+    },
+
+    disableXvfb: false,
+    ignoreAllFlags: false
+})
+  
+  // go to the york course website
+  await page.goto("https://w2prod.sis.yorku.ca/Apps/WebObjects/cdm",{waitUntil: 'networkidle0'});
+  
+  // // get unique link and create login link 
+  let links = await page.$$('a[href*="/Apps/WebObjects/cdm.woa/"]')
+  const loginLink = 'https://w2prod.sis.yorku.ca/Apps/WebObjects/cdm.woa/wa/loginppy?url=' + await links[0].evaluate(e1 => e1.getAttribute('href'));
+
+  //go to login page and switch to manual for login (possibly we can fix this).
+  await page.goto(loginLink, {waitUntil: 'networkidle0'});
+  await ensurePage(page, 'Passport York Login');
+  await page.type('#mli', 'calebwj'); //enters username
+  await page.type('#password', pass); //enters password
+  await sleep(150);                   //wait
+  await page.keyboard.press('Enter'); //press enter key
+  await sleep(60000);                 //allow time for 2fa
+
+  //reached the course page logged in. grab the link to the menu to return.
+  await ensurePage(page, "York University Courses Website - Search Courses by Subject");  
+  const menulink = await page.url();
+
+  //course link root
+  let clr = 'https://w2prod.sis.yorku.ca';
+
+  for (let i = 0; i < 219; i++) {
+    await page.waitForSelector('#subjectSelect');
+    await page.evaluate("document.getElementById('subjectSelect').value = " + i); //select Ith subject
+    await page.evaluate("document.getElementsByName('3.10.3.7')[0].click()");  //click th button
+    await page.waitForNavigation();                                                
+
+
+    //create the base of a link for a course in this subject
+    links = await page.$$('a[href*="/Apps/WebObjects/cdm.woa/"]');
+    for(link of links){
+      const text = await link.evaluate(e1 => e1.textContent);
+      if (text === "Fall/Winter 2024-2025 Course Schedule") {
+        clr += await link.evaluate(e1 => e1.getAttribute('href'));
+        break;
+      }
+    }
+    clr = clr.substring(0, clr.length - 5);
+
+    links = null;
+    
+    
+    //iterate through courses in the subject
+
+    let j = 0;
+    while (j < 12) {
+      
+      await page.goto(clr + j + '.0.5', {waitUntil: 'networkidle0'});
+      j++
+
+      await sleep(500);
+
+      //write something that resets J if read the same page twice. 
+      // then, write data grabbing here
+      // dont know where im storing it rn 
+      // at least it all works mashallah
+
+      
+      
+    }
+    //return to subject list
+    await page.goto(menulink, {waitUntil: 'networkidle0'});
+  }
+  
+  //end browser
+  await page.close();
+  await browser.close();
+}
+
+start();
---
+++ backend/src/firebase.js
@@ -0,0 +1,36 @@
+var admin = require("firebase-admin");
+var serviceAccount = require("./yorku-scheduler-firebase-adminsdk-fbsvc-7337973f11.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount)
+});
+
+const db = admin.firestore(); // Get Firestore reference
+
+
+//given a course's name and an array storing it's sections, upload this to the database.
+const addCourse = async (coursename, sections) => {
+  const docRef = db.collection('courses').doc(coursename);
+  await docRef.set({
+    sections: sections
+ });
+  console.log('Document written');
+};
+
+// const getDocument = async (collectionName, docId) => {
+//   try {
+//     const docRef = db.collection(collectionName).doc(docId);
+//     const doc = await docRef.get();
+//     if (doc.exists) {
+//       console.log('Document data:', doc.data());
+//     } else {
+//       console.log('No such document!');
+//     }
+//   } catch (error) {
+//     console.error('Error getting document:', error);
+//   }
+// };
+
+module.exports = {addCourse};
+
+
