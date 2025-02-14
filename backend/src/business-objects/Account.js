6eadf279cbf30801c34773b007fd915dfa4fc6bf
Zayn Ali <zayn.altamimi05@gmail.com>
2025-02-14 11:55:27 -0500

    Update Account.js for basic review feature

--- backend/src/business-objects/Account.js
+++ backend/src/business-objects/Account.js
@@ -2,16 +2,18 @@
  * Each account has 5 fields to store. The username, password, email, courses enrolled in, and friends.
  * Friends is another collection of friend objects, username, email, and password are strings, and enrolements is an array of specified courses (by section).
  */
-//const Course = require("./Course"); // for some imlpementation later
+const Course = require("./Course");
+const Review = require("./Review");
 
 class Account {
   // constructor for making an account
-  constructor(username, password, email, enrollments, friends) {
+  constructor(username, password, email, enrollments, friends, reviews = {}) {
     this.username = username;
     this.password = password;
     this.email = email;
     this.enrollments = enrollments;
     this.friends = friends;
+    this.reviews = reviews;
   }
 
   // method for adding a course
@@ -60,6 +62,27 @@ class Account {
   // method for getting email
   getEmail() {
     return this.email;
+  }
+
+  // method for adding a review for a course
+  addReview(courseCode, section, quality, difficulty, text) {
+    if (!this.reviews[courseCode]) {
+      this.reviews[courseCode] = [];
+    }
+    
+    const review = new Review(quality, difficulty, text);
+    this.reviews[courseCode].push(review);
+
+    // Assuming the course object has a method to add a review to the professor
+    const course = this.enrollments.find(c => c.courseCode === courseCode);
+    if (course) {
+      course.addReviewToProfessor(section, review); // Add the review to the professor
+    }
+  }
+
+  // method for getting reviews for a course
+  getReviews(courseCode) {
+    return this.reviews[courseCode] || [];
   }
 }
 
