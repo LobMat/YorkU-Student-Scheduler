18196ee42d512f8646e7065f0349dcda23477c14
Ahmet Karaca <ahmetkaraca.business@gmail.com>
2025-02-14 23:40:14 -0500

    Wrote account test cases

--- backend/test/models/Account.test.js
+++ backend/test/models/Account.test.js
@@ -1,4 +1,7 @@
-const Account = require("../src/business-objects/Account");
+const { default: test } = require("node:test");
+const Account = require("../../src/business-objects/Account");
+// const { get } = require("http");
+// const { AccountUtils } = require("../../src/models/Account");
 
 test("Create a new account", () => {
   let newAccount = new Account(
@@ -14,3 +17,52 @@ test("Create a new account", () => {
   expect(newAccount.getEnrolledCourses()).toEqual([]);
   expect(newAccount.getFriendList()).toEqual([]);
 });
+
+test("Change account password", () => {
+  let newAccount = new Account(
+    "ahmet",
+    "123pass",
+    "ahmetkrc@hotmail.com",
+    [],
+    []
+  );
+  newAccount.setPassword("newpass123");
+  expect(newAccount.getPassword()).toBe("newpass123");
+});
+
+test("Add a friend to the account", () => {
+  let newAccount = new Account(
+    "ahmet",
+    "123pass",
+    "ahmetkrc@hotmail.com",
+    [],
+    []
+  );
+  newAccount.addFriend("john");
+  expect(newAccount.getFriendList()).toContain("john");
+});
+
+test("Enroll in a course", () => {
+  let newAccount = new Account(
+    "ahmet",
+    "123pass",
+    "ahmetkrc@hotmail.com",
+    [],
+    []
+  );
+  newAccount.enrollCourse("EECS 2311");
+  expect(newAccount.getEnrolledCourses()).toContain("EECS 2311");
+});
+
+test("Check pending list", () => {
+  let newAccount = new Account(
+    "ahmet",
+    "123pass",
+    "ahmetkrc@hotmail.com",
+    [],
+    []
+  );
+  newAccount2.addPendingRequest("john");
+  expect(newAccount.getPendingRequests()).toContain("john");
+});
+
